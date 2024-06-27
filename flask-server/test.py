d4964c8ad0b7a54aabfb50091c759562e9546bf7
AyanT01 <abdulhaqayantayo@gmail.com>
2024-06-26 22:43:38 -0400

    Updated the test file

--- flask-server/test.py
+++ flask-server/test.py
@@ -1,3 +1,6 @@
-for i in range(100):
-    print(i) 
-
+def myfunction(param1,param2,param3):
+ a=param1+param2+param3
+ b = a* 2
+    if(b> 10): print('Result is greater than 10')
+    else: print('Result is 10 or less')
+return a, b
