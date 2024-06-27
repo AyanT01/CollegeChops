d6df0c8bf06be18166bd8c1717bcc5077a7884ba
EmmanuelHey <folafako05@gmail.com>
2024-06-27 00:29:46 -0500

    added dishes page

--- flask-server/app.py
+++ flask-server/app.py
@@ -67,5 +67,11 @@ def uploads():
 def success():
     return render_template("success.html")
 
+
+@app.route("/dishes/<int:dish_id>")
+def dish(dish_id):
+    recipe = Recipe.query.get_or_404(dish_id)
+    return render_template("recipes.html", recipe=recipe)
+
 if __name__ == '__main__':
     app.run(debug=True)
